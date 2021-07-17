468f0e40f4a6a1a362bf15a6aa3902b532772b20
Safiullah Khan <safiullah.khan145@gmail.com>
2021-07-17 18:01:37 +0500

    Database changed to sqlite3

--- MyCustomWidget/settings.py
+++ MyCustomWidget/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'MyCustomWidget.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mycustomwidget',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
